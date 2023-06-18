c3149ca1e2957c3770e714d0c7d85dd9935b2629
nobodyn0se <nobodyn0se@protonmail.ch>
2023-06-19 00:51:41 +0530

    Configured updation and deletion routes

--- backend/src/routes/li-routes.js
+++ backend/src/routes/li-routes.js
@@ -1,27 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const ListItem = require('../models/list-items');
+const ListItem = require("../models/list-items");
 
 // GET request to retrieve all list items
-router.route('').get((req, res) => {
-    ListItem.find()
-        .then((listItems) => res.json(listItems))
-        .catch((err) => res.status(400).json(`Error: ${err}`));
+router.route("").get((req, res) => {
+  ListItem.find()
+    .then((listItems) => res.json(listItems))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 // POST request to add a new list item
-router.route('/add').post((req, res) => {
-    const text = req.body.text;
-    const description = req.body.description;
+router.route("/add").post((req, res) => {
+  const text = req.body.text;
+  const description = req.body.description;
 
-    const newListItem = new ListItem({
-        text,
-        description,
-    });
+  const newListItem = new ListItem({
+    text,
+    description,
+  });
 
-    newListItem.save()
-        .then(() => res.json('New list item added'))
+  newListItem
+    .save()
+    .then(() => res.json("New list item added"))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// PUT request to update a list item
+router.route("/update/:uuid").put((req, res) => {
+    console.log(req.params.uuid);
+  ListItem.findOneAndUpdate({ uuid: req.params.uuid }, {
+        text: req.body.text,
+        description: req.body.description,
+  })
+    .then(() => res.json("List item updated"))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// DELETE request to delete a list item
+router.route("/delete/:uuid").delete((req, res) => {
+    ListItem.findOneAndDelete({ uuid: req.params.uuid })
+        .then(() => res.json("List item deleted"))
         .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
