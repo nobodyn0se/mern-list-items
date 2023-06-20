22c12eb09063a0d29cef060014a5649efb429d14
nobodyn0se <nobodyn0se@protonmail.ch>
2023-06-21 00:47:22 +0530

    Refactored login routes and token handling

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,68 +1,87 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const router = require('express').Router();
+const mongoose = require("mongoose");
+
+const router = require("express").Router();
 
 const secretKey = process.env.JWT_SECRET_KEY;
 
-const User = require('../models/users');
+const User = require("../models/users");
 
-router.post('/register', async (req, res) => {
-    try {
-        const { username, password } = req.body;
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-        if(!(username && password)) {
-            res.status(400).json({ message: 'Both username and password fields are required' });
-        }
+    if (!(username && password)) {
+      res
+        .status(400)
+        .json({ message: "Both username and password fields are required" });
+    }
 
-        const doesUserAlreadyExist = await User.findOne({ username });
+    const doesUserAlreadyExist = await User.findOne({ username });
 
-        if(doesUserAlreadyExist) {
-            res.status(400).json({ message: 'User already exists' });
-        }
+    if (doesUserAlreadyExist) {
+      res.status(400).json({ message: "User already exists" });
+    }
 
-        encryptedPassword = await bcrypt.hash(password, 16);
+    encryptedPassword = await bcrypt.hash(password, 16);
 
-        const user = await User.create({
-            username: username.toLowerCase(),
-            password: encryptedPassword,
-        });
+    const user = await User.create({
+      username: username.toLowerCase(),
+      password: encryptedPassword,
+    });
 
-        const token = jwt.sign({ username: user.username }, secretKey);
-        user.token = token;
+    const token = jwt.sign({ username: user.username }, secretKey);
 
-        res.status(201).json(user);
-    } catch (err) {
-        console.log(err);
-      }
+    //User.findOneAndUpdate({ username: user.username }, { token: token });
+
+    res
+      .status(201)
+      .json({
+        message: `User created successfully with the username: ${user.username}`,
+        token: token,
+      });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const { username, password } = req.body;
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-        if(!(username && password)) {
-            res.status(400).json({ message: 'Both username and password fields are required' });
-        }
-
-        const user = await User.findOne({ username });
-        
-        if(user === null) {
-            res.status(400).json({ message: 'User does not exist' });
-        }
-
-        if(user && (await bcrypt.compare(password, user.password))) {
-            const token = jwt.sign({ username: user.username }, secretKey);
-            user.token = token;
-
-            res.status(200).json(user);
-        }
-
-        res.status(400).json({ message: 'Invalid credentials' });
-    } catch (err) {
-        console.log(err);
+    if (!(username && password)) {
+      res
+        .status(400)
+        .json({ message: "Both username and password fields are required" });
     }
- });
+
+    let user = await User.findOne({ username });
+
+    if (user === null) {
+      res.status(400).json({ message: "User does not exist" });
+    }
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ username: user.username }, secretKey);
+
+      //User.findOneAndUpdate({ username: user.username }, { token: token });
+
+      res
+        .status(200)
+        .json({
+          message: "Successfully logged in",
+          username: user.username,
+          token: token,
+        });
+    } else {
+      res.status(400).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
