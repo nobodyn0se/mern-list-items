96f011ef454129c6a7d187f29e48ec6bc791912e
nobodyn0se <nobodyn0se@protonmail.ch>
2023-06-20 22:25:17 +0530

    Added login pathways to the app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const port = process.env.PORT || 6000;
 
@@ -14,15 +14,18 @@ app.use(express.json());
 const dbURI = process.env.MONGO_URI;
 
 mongoose
-    .connect(dbURI)
-    .then(console.log('Successfully connected to MongoDB cluster'))
-    .catch((err) => console.log(`Error while establishing connection with the database: ${err}`));
+  .connect(dbURI)
+  .then(console.log("Successfully connected to MongoDB cluster"))
+  .catch((err) =>
+    console.log(`Error while establishing connection with the database: ${err}`)
+  );
 
-const listItemsRouter = require('./routes/li-routes');
-app.use('/v1/list-items', listItemsRouter);
+const listItemsRouter = require("./routes/li-routes");
+app.use("/v1/list-items", listItemsRouter);
 
-app.listen(
-    port, () => {
-        console.log(`Server is up and running on port: ${port}`);
-    }
-);
+const loginRouter = require("./routes/login");
+app.use("/v1", loginRouter);
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port: ${port}`);
+});
