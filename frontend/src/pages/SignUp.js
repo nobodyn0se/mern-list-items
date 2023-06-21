17a8037539351cf73ca31f0f1203f585918d145b
nobodyn0se <nobodyn0se@protonmail.ch>
2023-06-22 03:40:32 +0530

    Integrated signup API with base UI

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router";
+
+import { useAuthProvider } from '../context/AuthContext';
 
 const SignUp = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+
+  const { signup, token, loading } = useAuthProvider();
+
+  const navigate = useNavigate();
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -17,18 +24,26 @@ const SignUp = () => {
     setConfirmPassword(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     if (password === confirmPassword) {
       
         //send data to server 
-        
+        await signup(username, password);
+
     } else {
       alert('Passwords do not match');
     }
   };
 
+  useEffect(() => {
+    if (token) {
+      navigate("/login");
+    }
+  }, [navigate, token]);
+
   return (
+    loading ? <p>Loading...</p> :
     <form onSubmit={handleSubmit}>
       <label>
         Username:
