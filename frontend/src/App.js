d9bc2c508031a27c206b2e674108b7ee82dcd976
nobodyn0se <nobodyn0se@protonmail.ch>
2023-06-21 22:02:12 +0530

    Set up routing for pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes, Navigate } from "react-router-dom";
+
+import LoginPage from "./pages/Login";
+import ListItems from "./pages/ListItems";
+import "./App.css";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<ListItems />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
