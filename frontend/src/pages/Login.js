cc0c228dd39aea62832189ec6b3562879f40669a
nobodyn0se <nobodyn0se@protonmail.ch>
2023-06-21 22:01:25 +0530

    Set up initial definition of sign-up and login pages

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    //send data to server
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Log In</button>
+   </form>
+  );
+}
+
+export default LoginPage;
---
+++ frontend/src/pages/SignUp.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      
+        //send data to server 
+        
+    } else {
+      alert('Passwords do not match');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <label>
+        Confirm Password:
+        <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
+
+export default SignUp;
