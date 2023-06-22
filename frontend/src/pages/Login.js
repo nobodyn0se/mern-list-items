6d679dd4291115e3eea701177181bceceb1c46ad
nobodyn0se <nobodyn0se@protonmail.ch>
2023-06-22 14:37:52 +0530

    Stylized the login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from "react";
 
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+} from "mdb-react-ui-kit";
+
 import { useNavigate } from "react-router";
 
 import { useAuthProvider } from "../context/AuthContext";
@@ -36,27 +46,55 @@ const LoginPage = () => {
   return loading ? (
     <p>Loading...</p>
   ) : (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </label>
-        <br />
-        <button onClick={handleSubmit} type="submit">
-          Log In
-        </button>
-      </form>
-    </>
+    <MDBContainer fluid>
+      <MDBRow className="d-flex justify-content-center align-items-center h-100">
+        <MDBCol col="12">
+          <MDBCard
+            className="bg-dark text-white my-5 mx-auto"
+            style={{ borderRadius: "1rem", maxWidth: "400px" }}
+          >
+            <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+              <h2 className="fw-bold mb-2">Login</h2>
+              <p className="text-white-50 mb-5">
+                Enter your username and password to continue
+              </p>
+
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-white"
+                label="Email address"
+                type="email"
+                size="lg"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                labelClass="text-white"
+                label="Password"
+                type="password"
+                size="lg"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+
+              <MDBBtn outline className="mx-2 px-5 my-3" color="white" size="lg">
+                Login
+              </MDBBtn>
+
+              <div>
+                <p className="mt-3">
+                  Create a new account<br />
+                  <a href="/signup" className="text-white-50 fw-bold">
+                    Sign Up
+                  </a>
+                </p>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
 
