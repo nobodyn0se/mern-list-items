2299c4486905bbb62fdaf6135ea2eeb83d6c87de
nobodyn0se <nobodyn0se@protonmail.ch>
2023-06-23 20:48:05 +0530

    Integrated update and delete APIs with UI

--- frontend/src/pages/ListItems.js
+++ frontend/src/pages/ListItems.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 import "../styles/ListItems.css";
@@ -10,12 +10,13 @@ import {
   MDBCol,
   MDBCard,
   MDBCardBody,
-  MDBInput,
+  MDBCardTitle,
   MDBIcon,
 } from "mdb-react-ui-kit";
 
 import { useListProvider } from "../context/ListContext";
 import { useAuthProvider } from "../context/AuthContext";
+import TodoController from "../components/ModalController";
 
 const ListItems = () => {
   const navigate = useNavigate();
@@ -23,8 +24,48 @@ const ListItems = () => {
   const { token } = useAuthProvider();
   const { getListItems, loading, addItem, itemList } = useListProvider();
 
-  const fetchList = async () => {
-    await addItem();
+  const [todoMode, setTodoMode] = useState("add");
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  // const fetchList = async () => {
+  //   await addItem();
+  // };
+
+  const toggleShow = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const handleClose = () => {
+    setModalOpen(false);
+    setSelectedTodo(null);
+    setTodoMode("add");
+  };
+
+  const handleDelete = (item) => {
+    setTodoMode("delete");
+    setSelectedTodo({
+      uuid: item.uuid,
+      text: item.text,
+      description: item.description,
+    });
+    toggleShow();
+  };
+
+  const handleEdit = (item) => {
+    setTodoMode("edit");
+    setSelectedTodo({
+      uuid: item.uuid,
+      text: item.text,
+      description: item.description,
+    });
+    toggleShow();
+  };
+
+  const handleAdd = () => {
+    setTodoMode("add");
+    setSelectedTodo(null);
+    toggleShow();
   };
 
   useEffect(() => {
@@ -44,47 +85,72 @@ const ListItems = () => {
   return loading ? (
     <p>Loading...</p>
   ) : (
-    <MDBContainer fluid>
-      <MDBRow className="d-flex justify-content-center align-items-center">
-        <MDBCol size="5" className="text-align-right">
-          <h4>Total List Items: {itemList.length}</h4>
-        </MDBCol>
-        <MDBCol size="5">
-          <MDBBtn outline className="mx-2 px-5 my-3 bg-dark text-white rounded" color="black" size="lg">
-            Logout
-          </MDBBtn>
-        </MDBCol>
-      </MDBRow>
-      <MDBRow className="w-100">
-        {itemList.map((item, index) => (
-          <MDBCol key={`${item.uuid}+${index}`} size="12" sm="6" md="4" lg="3">
-            <MDBCard key={item.uuid} className="my-2 d-flex flex-row">
-              <MDBIcon
-                icon="pencil"
-                className="align-self-center mx-3"
-                size="lg"
-              />
-              <MDBIcon
-                icon="trash"
-                className="align-self-center mx-2"
-                size="lg"
-              />
-              <MDBCardBody className="d-flex flex-column align-items-center mx-auto w-100">
-                <MDBCol>
-                  <MDBRow className="w-100 text-align-left">
-                    <h2 className="fw-bold mb-2 card-title">{item.title}</h2>
-                  </MDBRow>
-                  <MDBRow className="mb-0 w-100">
-                    <p>{item.description}</p>
-                  </MDBRow>
-                </MDBCol>
-              </MDBCardBody>
-            </MDBCard>
+    <>
+      <MDBContainer fluid>
+        <MDBRow className="d-flex justify-content-center align-items-center">
+          <MDBCol size="5" className="text-align-right">
+            <h4>Total List Items: {itemList.length}</h4>
           </MDBCol>
-        ))}
-      </MDBRow>
-      <button onClick={() => fetchList()}>Add a ToDo</button>
-    </MDBContainer>
+          <MDBCol size="5">
+            <MDBBtn
+              outline
+              className="mx-2 px-5 my-3 bg-dark text-white"
+              color="black"
+              size="lg"
+              rounded
+            >
+              Logout
+            </MDBBtn>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow className="w-100">
+          {itemList.map((item, index) => (
+            <MDBCol
+              key={`${item.uuid}+${index}`}
+              size="12"
+              sm="6"
+              md="4"
+              lg="3"
+            >
+              <MDBCard key={item.uuid} className="my-2 d-flex flex-row">
+                <MDBIcon
+                  icon="pencil"
+                  className="align-self-center mx-3"
+                  size="lg"
+                  onClick={() => handleEdit(item)}
+                  role="button"
+                />
+                <MDBIcon
+                  icon="trash"
+                  className="align-self-center mx-2"
+                  size="lg"
+                  onClick={() => handleDelete(item)}
+                  role="button"
+                />
+                <MDBCardBody className="px-3 pt-2 d-flex flex-column align-items-center mx-auto w-100">
+                        <div className="w-100 mb-0 fw-bold card-title text-no-wrap">
+                          {item.text}
+                        </div>
+                        <p className="w-100">{item.description}</p>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          ))}
+        </MDBRow>
+        <button onClick={handleAdd}>Add a ToDo</button>
+      </MDBContainer>
+      {modalOpen && (
+        <TodoController
+          open={modalOpen}
+          todoMode={todoMode}
+          todoItem={selectedTodo}
+          setShow={setModalOpen}
+          // toggleShow={toggleShow}
+          // handleEdit={handleEdit}
+          handleClose={handleClose}
+        />
+      )}
+    </>
   );
 };
 
